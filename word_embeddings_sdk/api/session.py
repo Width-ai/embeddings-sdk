128f03e21fb55b6c4b516855db86212823c5ff0a
pa-t <patrickhennis@gmail.com>
2023-10-26 09:59:55 -0500

    updates the base url to point at new prefix destination, bump version

--- word_embeddings_sdk/api/session.py
+++ word_embeddings_sdk/api/session.py
@@ -7,7 +7,7 @@ from word_embeddings_sdk.utils.utils import setup_logger
 
 
 class WordEmbeddingsSession:
-    def __init__(self, customer_id: str, api_key: str, url: str = "https://wordembeddings.ai"):
+    def __init__(self, customer_id: str, api_key: str, url: str = "https://www.wordembeddings.ai/api"):
         self.logger = setup_logger(__name__)
         self.api_url = url
         self.customer_id = customer_id
